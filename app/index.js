ef90a96f9a71af1a31baaa5fcd3a10ec71d52920
Martín <martinchogimenez@gmail.com>
2025-06-06 12:39:12 -0300

    Fixing index encoding code

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
   res.end('👋 ¡Hola desde Docker con Node.js!');
 });
 
